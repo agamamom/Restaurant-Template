a135a4cfe6832c49e70ebe2b2989af13c0f7380c
dathoang <dathoang31082001@gmail.com>
2022-09-22 22:54:27 +0700

    fix icon play video

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -7,7 +7,7 @@ const Intro = () => {
   const [playVideo, setPlayVideo] = useState(false)
   const videoRef = React.useRef();
   const handleVideo = () => {
-    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+    setPlayVideo((playVideo) => !playVideo);
     if (playVideo) {
       videoRef.current.play();
     } else {
@@ -19,7 +19,7 @@ const Intro = () => {
       <video src={meal} ref={videoRef} type="video/mp4" loop controls={false} muted />
       <div className="app__video-overlay flex__center">
         <div className="app__video-overlay_circle flex__center" onClick={handleVideo}>
-          {playVideo ? <BsFillPlayFill color="#fff" fontSize={30} /> : <BsPauseFill color='#fff' fontSize={30} />}
+          {!playVideo ? <BsPauseFill color="#fff" fontSize={30} /> : <BsFillPlayFill color='#fff' fontSize={30} />}
         </div>
       </div>
     </div>
